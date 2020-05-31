9413e9b2007eb8763b24443d40566492ab6d3359
Listya Widyasari <widyasari.listya@gmail.com>
2020-05-31 14:39:29 +0100

    Update mario.c

--- mario.c
+++ mario.c
@@ -3,24 +3,24 @@
 
 int main(void)
 {
-    
+
   int height;
   int x, y, z;
-    
+
   do
   {
-    printf ("Height: ");
-    height = get_int();
+
+    height = get_int("Height:");
     if( height == 0)
     {
       return 0;
-    }       
+    }
   }
-  
+
   while (height < 1 || height > 23);
-                
+
   for (y=0; y < height; y++)
-  { 
+  {
     for (z = (height-1); z > y; z--)
     {
       printf(" ");
@@ -28,7 +28,7 @@ int main(void)
     for(x = 0; x < (y+1); x++)
     {
       printf("#");
-    } 
-    printf("#\n"); 
-  }  
-}  
+    }
+    printf("#\n");
+  }
+}
